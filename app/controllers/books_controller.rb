fcf1b75d70cc8b101d1f896f9f221c451f28dbfe
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-05-25 02:24:26 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,18 +4,26 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+   book = Book.new(book_params)
+   book.save
+   redirect_to book_path(book.id)  
   end
   
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
   end
 
 private
