632450ba56fd46f4152de09a78da41e1ff2b3b14
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-05-27 15:03:23 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,13 @@ class BooksController < ApplicationController
   end
   
   def create
-   book = Book.new(book_params)
-   book.save
-   redirect_to book_path(book.id)  
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
   end
   
   def index
-    @books = Book.all
+    @books = Book.all  
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources:books, except: [:new,]
-  get '/' => 'books#new'
-  get 'top' => 'homes#top'
+  resources:books
+  get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
