24c86da0a37fea28930cc25b3f2a68f54b70ea88
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-05-31 14:20:33 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
    if @book.save
-    flash[:notice] = "successfully"
-    redirect_to book_path(book.id)  
+     flash[:notice] = "Book was successfully."
+    redirect_to book_path(@book.id)
    else
     @books = Book.all
     render :index
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
    if book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)  
+    redirect_to book_path(@book.id)  
    else
      @book = Book.new
      render :edit
