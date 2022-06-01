66c36014d8115790f8b4bfa1d342e2b561532e10
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-06-01 06:02:24 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
    if @book.save
-     flash[:notice] = "Book was successfully."
+     flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
    else
     @books = Book.all
@@ -28,20 +28,20 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-   if book.update(book_params)
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book.id)  
    else
-     @book = Book.new
      render :edit
    end
   end
 
   def destroy
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    book = Book.find(params[:id])  
+    book.destroy 
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'   
   end
   
 private
